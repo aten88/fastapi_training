0cd33137e902093533618c6de57492c29d8ddc41
Алексей Тен <virtuoso_ten@mail.ru>
2024-01-09 22:57:45 +0300

    Надо посмотреть еще раз Enum

--- main.py
+++ main.py
@@ -14,12 +14,17 @@ class EduactionLevel(str, Enum):
     HIGHER = 'Высшее образование'
 
 
-@app.get('/me')
+@app.get('/me', tags=['special methods'], summary='Приветствие автора')
 def hello_author():
     return {'Hello': 'author'}
 
 
-@app.get('/{name}')
+@app.get(
+        '/{name}',
+        tags=['common methods'],
+        summary='Общее приветствие',
+        response_description='Полная строка приветствия'
+    )
 def greetings(
     name: str,
     surname: str,
@@ -27,6 +32,14 @@ def greetings(
     is_staff: bool = False,
     education_level: Optional[EduactionLevel] = None,
 ) -> dict[str, str]:
+    """
+        Приветствие пользователя:
+        - **name**: имя
+        - **surname**: фамилия
+        - **age**: возраст (опционально)
+        - **is_staff**: является ли пользователь сотрудником
+        - **education_level**: уровень образования (опционально)
+    """
     result = ' '.join([name, surname])
     result = result.title()
     if age is not None:
